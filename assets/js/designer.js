175e76cac4a49bcf0e139f40e43be1b2a1060762
Dennis Wells <56503655+Dennis231708@users.noreply.github.com>
2019-12-12 08:52:37 -0600

    added footer and fixed nav responsiveness

--- assets/js/designer.js
+++ assets/js/designer.js
@@ -1,19 +1,4 @@
-// $('.dropdown').mouseenter(function(){
-//     $(this).children('.dropdown .dropdown-menu').slideDown('slow');
-//     $(this).children('.dropdown .dropdown-menu').toggleClass('open');
-
-//     $(this).mouseleave(function(){
-//         $(this).children('.dropdown .dropdown-menu').stop(true).slideUp('fast');
-//         $(this).children('.dropdown .dropdown-menu').removeClass('open');
-
-
-//     });
-// });
-
-
-
-
-
+// Navbar hover on desktop click on mobile
 $(document).ready(function(){
     function windowSizeCheck(){
        var $windowSize = $(window).width();
@@ -29,17 +14,26 @@ $(document).ready(function(){
         });
        }
        else{
-        $('.dropdown').on('click', function(){
-            $(this).children('.dropdown .dropdown-menu').slideDown('slow');
-            $(this).children('.dropdown .dropdown-menu').toggleClass('open');
-            $(this).css({'max-height': '400px', 'overflow-y': 'auto'})
-        
-            $(this).on('click', function(){
-                $(this).children('.dropdown .dropdown-menu').stop(true).slideUp('fast');
-                $(this).children('.dropdown .dropdown-menu').toggleClass('open');       
-            });
-        });
+           
+      $('.dropdown').unbind("mouseenter");
+      $('.dropdown').unbind("mouseleave");
+
+        $('.dropdown').on('click', function() {
+                    $(this).children('.dropdown .dropdown-menu').slideDown('slow');
+                    $(this).children('.dropdown .dropdown-menu').toggleClass('openNav');
+
+                    if ($(this).children('.dropdown .dropdown-menu').attr('style')) {
+                      $(this).children('.dropdown .dropdown-menu').removeAttr('style');
+                    }
+            
+                  });
        }
     }
     windowSizeCheck();
+    $(window).on('resize', function() {
+        // this runs when document loads for the first time
+        windowSizeCheck();
+      });
+    
 });
+
